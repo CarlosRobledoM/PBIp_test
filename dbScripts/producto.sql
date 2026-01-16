d5716926a04dd5bb4dfcf7a8d712e4568a756a79
Carlos Felipe Robledo Murcia <carlos.robledo@olsoftware.com>
2026-01-16 11:22:16 -0500

    🚧 Correción de tablas y relaciones para continuar con metricas

--- dbScripts/producto.sql
+++ dbScripts/producto.sql
@@ -5,9 +5,16 @@ SELECT
     p.Manufacturer AS Fabricante,
     p.Brand AS Marca,
     p.Color,
-    p.Weight AS Peso,
+    p.Weight AS PesoOriginal,
+    p.WeightUnit AS UnidadPesoOriginal,
+    CASE 
+        WHEN p.WeightUnit = 'ounces' THEN p.Weight * 0.0283495
+        ELSE p.Weight
+    END AS PesoKilogramos,
+    p.SubCategoryKey,
+    p.CategoryKey,
     sc.SubCategoryName AS SubCategoria,
     c.CategoryName AS Categoria
 FROM [Data].Product p
 LEFT JOIN [Data].SubCategory sc ON p.SubCategoryKey = sc.SubCategoryKey
-LEFT JOIN [Data].Category c ON sc.CategoryKey = c.CategoryKey;
+LEFT JOIN [Data].Category c ON p.CategoryKey = c.CategoryKey;
